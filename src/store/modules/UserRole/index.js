b504ffbdd363e789f17659392e4d8b5d394027e8
Daneris <danerismendoza096@gmail.com>
2023-09-09 22:07:33 +0800

    Table::Applicants: approve and dissaprove working

--- src/store/modules/UserRole/index.js
+++ src/store/modules/UserRole/index.js
@@ -20,6 +20,25 @@ export default {
     },
 
     actions: {
+        APPROVE_USER_ROLE({commit},payload){
+            return new Promise((resolve,reject)=>{
+            UserRole.ApproveUserRole(payload).then((response)=>{
+                resolve(response.data)
+              }).catch((error)=>{
+                  reject(error)
+              });
+            })
+        },
+    
+        DISAPPROVE_USER_ROLE({commit},payload){
+            return new Promise((resolve,reject)=>{
+            UserRole.DissaproveUserRole(payload).then((response)=>{
+                resolve(response.data)
+              }).catch((error)=>{
+                  reject(error)
+              });
+            })
+        },
         
         GET_USER_ROLE_WITH_ACCESSESS_AND_REQUIREMENTS({commit}){
             return new Promise((resolve,reject)=>{
@@ -31,6 +50,7 @@ export default {
               });
             })
         },
+
         GET_APPLICANTS({commit}){
             return new Promise((resolve,reject)=>{
             UserRole.GetApplicants().then((response)=>{
@@ -41,6 +61,7 @@ export default {
               });
             })
         },
+
         GET_USER_ROLE({commit}){
             return new Promise((resolve,reject)=>{
             UserRole.GET_USER_ROLE().then((response)=>{
@@ -51,6 +72,7 @@ export default {
               });
             })
         },
+
         UPDATE_USER_ROLE_BY_ID({commit},payload){
             return new Promise((resolve,reject)=>{
             UserRole.UPDATE_USER_ROLE_BY_ID(payload).then((response)=>{
