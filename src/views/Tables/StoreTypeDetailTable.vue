9e956f82bb2fc5f4ce5c324b2e3987c9a0c89f09
nicolelfporca <113660629+nicolelfporca@users.noreply.github.com>
2023-09-16 13:05:52 +0800

    route:index - add %20 in path name

---
+++ src/views/Tables/StoreTypeDetailTable.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <br><br>
+        <h1>tableeeeeeeee</h1>
+        <!-- <v-data-table
+            :headers="headers"
+            :items="USER_ROLES"
+        >
+        </v-data-table> -->
+
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                headers: [
+                    { text: "User-Role", align: "center",  sortable: false },
+                    { text: "Requirements", align: "center", sortable: false },
+                    { text: "Access", align: "center", sortable: false },
+                    { text: 'Actions', align: "center", value: 'actions', sortable: false },
+                    ],
+                } 
+            },
+        mounted(){
+            console.log('hello')
+            console.log('hello2')
+            // this.$store.dispatch("GET_USER_ROLE_WITH_ACCESSESS_AND_REQUIREMENTS")
+        }
+
+    }
+</script>
