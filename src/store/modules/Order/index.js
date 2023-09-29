304009f9aabc2fdcfe50e8426b00dd72c6023223
Daneris Mendoza <danerismendoza096@gmail.com>
2023-09-29 15:25:14 +0800

    Order Module: working

--- src/store/modules/Order/index.js
+++ src/store/modules/Order/index.js
@@ -4,16 +4,19 @@ export default {
     state: {
         ORDERS: [],
         SELECTED_ORDER_DETAILS: null,
+        ORDERS_TABLE_MODE: null,
     },
 
     getters: {
         ORDERS: (state) => state.ORDERS,
         SELECTED_ORDER_DETAILS: (state) => state.SELECTED_ORDER_DETAILS,
+        ORDERS_TABLE_MODE: (state) => state.ORDERS_TABLE_MODE,
     },
 
     mutations: {
         ORDERS: (state, data) => { state.ORDERS = data },
         SELECTED_ORDER_DETAILS: (state, data) => { state.SELECTED_ORDER_DETAILS = data },
+        ORDERS_TABLE_MODE: (state, data) => { state.ORDERS_TABLE_MODE = data },
     },
 
     actions: {
@@ -30,6 +33,16 @@ export default {
         ORDER({ commit }, payload) {
             return new Promise((resolve, reject) => {
                 Order.Order(payload).then((response) => {
+                    resolve(response.data)
+                }).catch((error) => {
+                    reject(error)
+                });
+            })
+        },
+        GET_ORDERS_BY_USER_ID({ commit }) {
+            return new Promise((resolve, reject) => {
+                Order.GetOrdersByUserId().then((response) => {
+                    commit('ORDERS', response.data)
                     resolve(response.data)
                 }).catch((error) => {
                     reject(error)
