b82591b10320e9825c285c965498d5e25a1c62f4
nicolelfporca <113660629+nicolelfporca@users.noreply.github.com>
2023-10-04 14:33:53 +0800

    SideNav: console bug fixed

--- src/views/components/SideMenuBar.vue
+++ src/views/components/SideMenuBar.vue
@@ -1,33 +1,39 @@
 <template>
   <div>
-    <v-list dense v-model="drawer" :mini-variant="isMobile">
-      <v-list-item>
-        <v-list-title>
-          <h2 class="epalengke-title">E-PALENKE ADMIN</h2>
-        </v-list-title>
-      </v-list-item>
-    </v-list>
- 
+    <v-app-bar elevation="0" color="secondary">
+      <h1>E-PALENGKE</h1>
+    </v-app-bar>
+
     <v-divider></v-divider>
 
-    <v-list>
-      <v-list-content
-        v-for="side_nav in SIDE_NAV"
-        :key="side_nav.id"
-        @click="navigateToRoute(side_nav.name, side_nav.id)"
-      >
-        <v-list-item :to="{ name: side_nav.name, params: { id: side_nav.id } }">
-          {{ side_nav.name }}
+    <v-list v-model="drawer" :mini-variant="isMobile">
+      <v-list>
+        <v-list-item
+          v-for="side_nav in SIDE_NAV"
+          :key="side_nav.id"
+          @click="navigateToRoute(side_nav.name, side_nav.id)"
+          :class="{ 'active-item': isParentActive(side_nav.name) }"
+        >
+          <v-list-item-content>
+            <router-link
+              :to="{ name: side_nav.name, params: { id: side_nav.id } }"
+              class="white--text side_nav_link"
+              exact-active-class=""
+            >
+              {{ side_nav.name }}
+            </router-link>
+          </v-list-item-content>
         </v-list-item>
-      </v-list-content>
-      <v-list-item @click="submitLogout()"> LOGOUT </v-list-item>
+        <v-list-item @click="submitLogout()">
+          <v-list-item-title>LOGOUT</v-list-item-title>
+        </v-list-item>
+      </v-list>
     </v-list>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-
 export default {
   data() {
     return {
@@ -53,6 +59,11 @@ export default {
     navigateToRoute(routeName, id) {
       this.$router.push({ name: routeName, params: { id } });
     },
+
+    isParentActive(routeName) {
+      // Check if any child route of the given parent route is active
+      return this.$route.matched.some((route) => route.name === routeName);
+    },
   },
   computed: {
     ...mapGetters(["SIDE_NAV", "USER_DETAILS"]),
@@ -71,7 +82,11 @@ export default {
 </script>
 
 <style scoped>
-.epalengke-title {
-  margin: 6px 0 5px;
+.side_nav_link {
+  text-decoration: none;
+}
+
+.active-item {
+  background-color: #606060; /* Change to the color you want for the active item */
 }
 </style>
