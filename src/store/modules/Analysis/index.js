cc58acb8466a501d74bf495ddec71bc42d2654b2
Daneris Mendoza <danerismendoza096@gmail.com>
2023-11-25 14:46:02 +0800

    Home: Order Analysis added

--- src/store/modules/Analysis/index.js
+++ src/store/modules/Analysis/index.js
@@ -4,16 +4,19 @@ export default {
     state: {
         USER_ROLES_ANALYSIS: null,
         USER_ROLES_STATUS_ANALYSIS: null,
+        ORDERS_ANALYSIS: null,
     },
 
     getters: {
         USER_ROLES_ANALYSIS: (state) => state.USER_ROLES_ANALYSIS,
         USER_ROLES_STATUS_ANALYSIS: (state) => state.USER_ROLES_STATUS_ANALYSIS,
+        ORDERS_ANALYSIS: (state) => state.ORDERS_ANALYSIS,
     },
 
     mutations: {
         USER_ROLES_ANALYSIS: (state, data) => { state.USER_ROLES_ANALYSIS = data },
         USER_ROLES_STATUS_ANALYSIS: (state, data) => { state.USER_ROLES_STATUS_ANALYSIS = data },
+        ORDERS_ANALYSIS: (state, data) => { state.ORDERS_ANALYSIS = data },
     },
 
     actions: {
@@ -31,6 +34,16 @@ export default {
             return new Promise((resolve, reject) => {
                 api.get('api/GET_USER_ROLES_STATUS_ANALYSIS').then((response) => {
                     commit('USER_ROLES_STATUS_ANALYSIS', response.data)
+                    resolve(response.data)
+                }).catch((error) => {
+                    reject(error)
+                });
+            })
+        },
+        GET_ORDERS_ANALYSIS({ commit }, payload) {
+            return new Promise((resolve, reject) => {
+                api.get('api/GET_ORDERS_ANALYSIS').then((response) => {
+                    commit('ORDERS_ANALYSIS', response.data)
                     resolve(response.data)
                 }).catch((error) => {
                     reject(error)
--- src/views/SideNav/Home.vue
+++ src/views/SideNav/Home.vue
@@ -3,15 +3,59 @@
     <v-container>
       <v-card class="home">
         <h2 class="font-weight-bold mb-3">{{ $route.name }}</h2>
+        <v-row>
+          <v-col cols="12">
+            <!-- Bar Graph for ORDERS_ANALYSIS -->
+            <apexchart type="bar" :options="chartOptionsOrders" :series="seriesOrders" />
+          </v-col>
+        </v-row>
       </v-card>
     </v-container>
   </v-app>
 </template>
 
 <script>
-import Login from "../components/Login.vue";
+import { mapGetters } from "vuex";
 export default {
-  components: { Login },
+  data() {
+    return {
+      ordersAnalysisData: [],
+    };
+  },
+  computed: {
+    ...mapGetters(["ORDERS_ANALYSIS"]),
+    chartOptionsOrders() {
+      // Calculate the height based on the number of data points
+      const chartHeight = Math.max(300, this.ordersAnalysisData.length * 30);
+
+      return {
+        xaxis: {
+          categories: this.ordersAnalysisData.map((item) => item.name),
+        },
+        chart: {
+          height: chartHeight + 'px', // Set the chart height
+          type: 'bar',
+        },
+      };
+    },
+    seriesOrders() {
+      return [{
+        name: 'Orders',
+        data: this.ordersAnalysisData.map((item) => item.count),
+      }];
+    },
+  },
+  methods: {
+    async fetchData() {
+      await this.$store.dispatch("GET_ORDERS_ANALYSIS").then(() => {
+        console.log(this.ORDERS_ANALYSIS);
+      });
+      this.ordersAnalysisData = this.ORDERS_ANALYSIS;
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
 };
 </script>
 
