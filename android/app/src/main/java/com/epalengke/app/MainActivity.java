f7843edd3524abbcee0e6b78b463d100aba106f3
Daneris Mendoza <danerismendoza096@gmail.com>
2023-12-11 08:28:36 +0800

    with toast

---
+++ android/app/src/main/java/com/epalengke/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.epalengke.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/views/SideNav/Login.vue
+++ src/views/SideNav/Login.vue
@@ -1,12 +1,20 @@
 <template>
     <v-app>
-        <Login/>
+        <Login />
     </v-app>
 </template>
 
 <script>
-    import Login from '../components/Login.vue';
-    export default{
-        components: { Login}
+import Login from '../components/Login.vue';
+import '@capacitor/toast';
+import '@capacitor/geolocation';
+export default {
+    components: { Login },
+    async mounted() {
+        const a = await Capacitor.Plugins.Geolocation.getCurrentPosition();
+        Capacitor.Plugins.Toast.show({
+            text: 'Location'+a.coords.latitude,
+        });
     }
+}
 </script>
